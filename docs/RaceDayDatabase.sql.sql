e1d38e69a1ae2028141bac511a14141638a8e7ba
Gomolemobig15 <gphenyo57@gmail.com>
2026-09-04 17:35:03 +0200

    Add files via upload

---
+++ docs/RaceDayDatabase.sql.sql
@@ -0,0 +1,8 @@
+INSERT INTO Enrolments
+    (ParticipantID, CategoryID, Status, RaceNumber)
+VALUES
+    (3, 1, 'Confirmed', 'JR1001'),
+    (3, 4, 'Confirmed', 'CR2001'),
+    (4, 2, 'Confirmed', 'JR1002'),
+    (4, 5, 'Confirmed', 'DW3001');
+GO
